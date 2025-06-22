6ebe9d3be0f11c53c4cc5e1a5b5ed51c7f558811
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-22 13:53:51 +0500

    Refactor Header component for improved layout and styling; update default layout to ensure full height and apply light theme.

--- components/Header.vue
+++ components/Header.vue
@@ -18,12 +18,19 @@
       </div>
     </div>
     <div class="w-full px-6 py-2">
-      <div class="container mx-auto">
-        <div class="border p-3">
-          <img src="../assets/icons8-menu.svg" alt="logo" class="h-10" />
+      <div class="container mx-auto flex">
+        <div class="flex items-center justify-center">
+          <img
+            src="../assets/icons8-menu.svg"
+            alt="logo"
+            class="border h-8 p-2 rounded-lg border-gray-300"
+          />
         </div>
-        <div class="">
-          <h1 class="text-3xl">Daily Herald</h1>
+        <div class="flex flex-col flex-1 items-center justify-center">
+          <h1 class="text-2xl font-bold">Daily Herald</h1>
+          <p class="text-sm text-base-content/80">
+            TRUSTED NEWS • GLOBAL PERSPECTIVE
+          </p>
         </div>
       </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-screen" data-theme="light">
     <Header />
     <main>
       <slot />
