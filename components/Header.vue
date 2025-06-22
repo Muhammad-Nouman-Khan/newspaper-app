ade3cab6e3d3f1732f97e2b2436eab06fd535620
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-22 14:23:41 +0500

    Update Header component styling to increase padding and add a bottom border for improved visual separation and layout consistency.

--- components/Header.vue
+++ components/Header.vue
@@ -17,7 +17,7 @@
         </div>
       </div>
     </div>
-    <div class="w-full px-6 py-2">
+    <div class="w-full px-6 py-6 border-b border-base-300">
       <div class="container mx-auto flex">
         <div class="flex md:hidden items-center justify-center">
           <img
